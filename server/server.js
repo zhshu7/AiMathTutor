abb941549850168f29bf6eb2668f696c63549a49
zhshu7 <93456391+zhshu7@users.noreply.github.com>
2023-02-26 00:32:40 -0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hi!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `I want you to act as a math tutor. I will write you a theorem and you will give at least one example and solutions to the examples, fully explain the examples. All eqations should be written in mathjax format. I also require you to explain in the laguage the theorem is given. \n please explain ${prompt}:`,
+      temperature: 0.7, 
+      max_tokens: 3000,
+      top_p: 1, 
+      frequency_penalty: 0.0, 
+      presence_penalty: 0, 
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(3000, () => console.log('AI server started on http://localhost:3000'))
