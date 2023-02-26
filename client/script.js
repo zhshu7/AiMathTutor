bba09699b15d0a0735fa9d1eea80498d8c343b89
zhshu7 <93456391+zhshu7@users.noreply.github.com>
2023-02-26 01:20:53 -0800

    update server url

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-math-tutor.onrender.com', {
+    const response = await fetch('http://localhost:3000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
